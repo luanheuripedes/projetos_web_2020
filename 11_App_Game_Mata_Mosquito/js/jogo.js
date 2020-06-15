91bfecf8a7dd62a48c7f9ab4f04a8ba74caa3e7a
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-15 17:43:33 -0300

    Tamanhos randomicos para o mosquito

--- 11_App_Game_Mata_Mosquito/js/jogo.js
+++ 11_App_Game_Mata_Mosquito/js/jogo.js
@@ -1,7 +1,50 @@
 /*Função que define o tamanho e a largura da tela e a atualiza caso o tamanho e a largura mudem*/
 var altura = 0, largura = 0;
 function ajustaTamanhoPalcoJogo(){
-    altura = window.innerHeight;
     largura = window.innerWidth;
-    console.log(altura, largura);
+    altura = window.innerHeight;
+    
+    console.log(largura, altura);
+}
+
+ajustaTamanhoPalcoJogo();
+
+function posicaoRandomica(){
+
+    /*Criado uma variavel para criar posições aleatorias para o mosquito*/ 
+    var posicaoX = Math.floor(Math.random() * largura) -90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX,posicaoY);
+
+    //criar o elemento html
+    var mosquito = document.createElement('img');
+    mosquito.src = '/img/mosca.png';
+    mosquito.className = tamanhoAleatorio();
+
+    //
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito);
+
+    
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3);
+    console.log(classe);
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
 }
