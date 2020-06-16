18c956a6c742ff02d3e51704aa5e8058dc834f96
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-16 11:28:01 -0300

    Criado a logica para criar e remover o elemento mosquito da tela

--- 11_App_Game_Mata_Mosquito/js/jogo.js
+++ 11_App_Game_Mata_Mosquito/js/jogo.js
@@ -11,6 +11,11 @@ ajustaTamanhoPalcoJogo();
 
 function posicaoRandomica(){
 
+    //remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+
     /*Criado uma variavel para criar posições aleatorias para o mosquito*/ 
     var posicaoX = Math.floor(Math.random() * largura) -90;
     var posicaoY = Math.floor(Math.random() * altura) - 90;
@@ -28,7 +33,8 @@ function posicaoRandomica(){
     //
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.top = posicaoY + 'px';
-    mosquito.style.position = 'absolute'
+    mosquito.style.position = 'absolute';
+    mosquito.id = 'mosquito';
 
     document.body.appendChild(mosquito);
 
