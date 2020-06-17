79a7a085a3c890c951bc0c438422f2a429d5b214
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-17 11:40:55 -0300

    criado tela de game over

--- 11_App_Game_Mata_Mosquito/js/jogo.js
+++ 11_App_Game_Mata_Mosquito/js/jogo.js
@@ -1,39 +1,41 @@
 /*Função que define o tamanho e a largura da tela e a atualiza caso o tamanho e a largura mudem*/
 var altura = 0, largura = 0, vidas = 1;
-function ajustaTamanhoPalcoJogo(){
+function ajustaTamanhoPalcoJogo() {
     largura = window.innerWidth;
     altura = window.innerHeight;
-    
+
     console.log(largura, altura);
 }
 
 ajustaTamanhoPalcoJogo();
 
-function posicaoRandomica(){
+function posicaoRandomica() {
 
     //remover o mosquito anterior (caso exista)
-    if(document.getElementById('mosquito')){
+    if (document.getElementById('mosquito')) {
+
         document.getElementById('mosquito').remove();
 
         //console.log('Elemento selecionado foi: v' + vidas);
-        if(vidas > 3){
+        if (vidas > 3) {
             alert('Interromper o jogo GAME OVER');
+            window.location.href = 'fim_de_jogo.html'
         }
-        else{
-        document.getElementById('v' + vidas).src = "/img/coracao_vazio.png";
+        else {
+            document.getElementById('v' + vidas).src = "/img/coracao_vazio.png";
         }
 
         vidas++;
     }
 
-    /*Criado uma variavel para criar posições aleatorias para o mosquito*/ 
-    var posicaoX = Math.floor(Math.random() * largura) -90;
+    /*Criado uma variavel para criar posições aleatorias para o mosquito*/
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
     var posicaoY = Math.floor(Math.random() * altura) - 90;
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX;
     posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-    console.log(posicaoX,posicaoY);
+    console.log(posicaoX, posicaoY);
 
     //criar o elemento html
     var mosquito = document.createElement('img');
@@ -45,7 +47,7 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
     mosquito.id = 'mosquito';
-    mosquito.onclick = function(){
+    mosquito.onclick = function () {
         this.remove();
     }
 
@@ -53,14 +55,14 @@ function posicaoRandomica(){
 
     console.log(ladoAleatorio());
 
-    
+
 }
 
-function tamanhoAleatorio(){
+function tamanhoAleatorio() {
     var classe = Math.floor(Math.random() * 3);
     console.log(classe);
 
-    switch(classe){
+    switch (classe) {
         case 0:
             return 'mosquito1'
         case 1:
@@ -70,10 +72,10 @@ function tamanhoAleatorio(){
     }
 }
 
-function ladoAleatorio(){
+function ladoAleatorio() {
     var lado = Math.floor(Math.random() * 2);
 
-    switch(lado){
+    switch (lado) {
         case 0:
             return 'ladoA'
         case 1:
