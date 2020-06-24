10646145913d448df84ee2936d5098b50807f619
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-24 18:40:47 -0300

    Deixando as cores do modal dinamico, de acordo com a funcao validarDados()

--- 12_App_Orcamento_Pessoal/app.js
+++ 12_App_Orcamento_Pessoal/app.js
@@ -8,9 +8,9 @@ class Despesa {
         this.valor = valor;
     }
 
-    validarDados(){
-        for(let i in this){ //eu percorro todos os atributos da classe despesa
-            if(this[i] == undefined || this[i] == '' || this[i] == null){
+    validarDados() {
+        for (let i in this) { //eu percorro todos os atributos da classe despesa
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false;
             }
         }
@@ -36,7 +36,7 @@ class Bd {
 
         localStorage.setItem(id, JSON.stringify(d))
 
-        localStorage.setItem('id',id);
+        localStorage.setItem('id', id);
     }
 }
 
@@ -59,13 +59,28 @@ function cadastrarDespesa() {
         valor.value
     );
 
-    if(despesa.validarDados()){
+    if (despesa.validarDados()) {
         bd.gravar(despesa);
+
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso';
+        document.getElementById('modal_titulo_div').className ='modal-header text-success';
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!';
+        document.getElementById('modal_btn').innerHTML = 'Voltar';
+        document.getElementById('modal_btn').className = 'btn btn-success';
+        
         //mensagem de sucesso
-        $('#sucessoGravacao').modal('show');
-    }else{
-        $('#erroGravacao').modal('show');//
+        $('#modalRegistraDespesa').modal('show');
+    } else {
+        
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro';
+        document.getElementById('modal_titulo_div').className ='modal-header text-danger';
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos';
+        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_btn').className = 'btn btn-danger';
+
+        //dialogo erro
+        $('#modalRegistraDespesa').modal('show');//
     }
-    
+
 }
 
