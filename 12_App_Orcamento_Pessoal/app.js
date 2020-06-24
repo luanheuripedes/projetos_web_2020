0371268a20566789fd442a68668c2595114007cf
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-24 15:59:17 -0300

    criado a classe bd e indices dinamicos

--- 12_App_Orcamento_Pessoal/app.js
+++ 12_App_Orcamento_Pessoal/app.js
@@ -1,5 +1,5 @@
-class Despesa{
-    constructor(ano,mes,dia,tipo,descricao,valor){
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano;
         this.mes = mes;
         this.dia = dia;
@@ -9,8 +9,31 @@ class Despesa{
     }
 }
 
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id');
 
-function cadastrarDespesa(){
+        if (id === null) {
+            localStorage.setItem('id', 0);
+        }
+    }
+    getProximoId() {
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId();
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id',id);
+    }
+}
+
+let bd = new Bd();
+
+function cadastrarDespesa() {
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
     let dia = document.getElementById('dia');
@@ -19,7 +42,7 @@ function cadastrarDespesa(){
     let valor = document.getElementById('valor');
 
     let despesa = new Despesa(
-        ano.value, 
+        ano.value,
         mes.value,
         dia.value,
         tipo.value,
@@ -27,10 +50,6 @@ function cadastrarDespesa(){
         valor.value
     );
 
-    gravar(despesa);
+    bd.gravar(despesa);
 }
 
-function gravar(d){
-    
-    localStorage.setItem('despesa', JSON.stringify(d))//IDENTIFICAÇÃO E DADO QUE NOS QUEREMOS ARMAZENAR
-}
