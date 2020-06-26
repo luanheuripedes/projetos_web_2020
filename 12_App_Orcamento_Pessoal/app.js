1e145f046134d4c9dea4e9de084ce95f5bdb89d5
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-26 20:55:02 -0300

    conclusao do projeto

--- 12_App_Orcamento_Pessoal/app.js
+++ 12_App_Orcamento_Pessoal/app.js
@@ -58,6 +58,8 @@ class Bd {
                 continue
             }
 
+            despesa.id = i;
+
             //adicionando no meu array despesas a despesa recuperada no localStorage
             despesas.push(despesa);
         }
@@ -110,6 +112,10 @@ class Bd {
 
         console.log(despesasFiltradas)
         return despesasFiltradas;
+    }
+
+    remover(id){
+        localStorage.removeItem(id);
     }
 }
 
@@ -217,6 +223,25 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        //criar o botao de exclusao
+        let btn = document.createElement("button");
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = '<i class="fas fa-times"> </i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            //remover despesa;
+            let id = this.id.replace('id_despesa_', '');
+
+            //alert(id);
+
+            bd.remover(id);
+
+            window.location.reload();
+        }
+        linha.insertCell(4).append(btn);
+
+        console.log(d);
     })
 }
 
