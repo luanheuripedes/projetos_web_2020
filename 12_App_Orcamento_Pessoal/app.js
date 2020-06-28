c7fcb08e5768fbb4bb21cafda675c11369fe5126
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-06-28 14:43:27 -0300

    Adicionado o modal de exclusão com as opções "SIM e NAO"

--- 12_App_Orcamento_Pessoal/app.js
+++ 12_App_Orcamento_Pessoal/app.js
@@ -251,9 +251,8 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
                  window.location.reload();
             }
         }
-        linha.insertCell(4).append(btn);
 
-        console.log(d);
+        linha.insertCell(4).append(btn);
     })
 }
 
